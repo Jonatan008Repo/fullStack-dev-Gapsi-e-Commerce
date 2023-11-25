659a750db27894437b8c92270f4cabb4cf1f0172
Jonatan008Repo <vergara_jona@hotmail.com>
2023-11-25 08:57:51 -0600

    add routes api

---
+++ back/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const appApi = require('./routers/app.js');
+const cors = require("cors");
+//const http = require("http");
+const app = express();
+const port = 3000;
+
+const whitelist = ["http://localhost:3001", "http://localhost:3000",]
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("no permitido"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(options));
+app.use(express.json());
+appApi(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}/GapsiECommerce/Api/ver`);
+}
+);
+
